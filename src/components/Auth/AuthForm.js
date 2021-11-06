f8264912d1f9bfcf6aa74bba62cac21f005caf90
Prabhjot Singh <prabhjotsingh394@gmail.com>
2021-11-06 21:39:15 +0530

    added user sign in

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -22,39 +22,46 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
-
+    let url;
     if(isLogin) {
-
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDKVPw-pcqAPph6OghWliPzNd06gOcSN14';
     } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKVPw-pcqAPph6OghWliPzNd06gOcSN14',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email:enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKVPw-pcqAPph6OghWliPzNd06gOcSN14';
+    }
+
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email:enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
         }
-      ).then(res => {
+      }
+    ).then(res => {
       setIsLoading(false);
 
-        if(res.ok) {
-
-        } else {
-          return res.json().then((data) => {
-            console.log(data);
-            let errorMessage = 'Authentication failed!';
-            if(data && data.error && data.error.message) {
-              errorMessage = data.error.message;
-            }
-            alert(errorMessage);
-          });
-        }
-      });
-    }
+      if(res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          console.log(data);
+          let errorMessage = 'Authentication failed!';
+          // if(data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+          alert(errorMessage);
+          throw new Error(errorMessage);
+        });
+      }
+    }).then((data) => {
+      console.log(data);
+    }).catch((err)=>{
+      alert(err.message);
+    });
 
   };
 
