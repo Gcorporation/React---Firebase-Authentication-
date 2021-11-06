c79a47ef8a221793178f8626ce50f645a3621425
Prabhjot Singh <prabhjotsingh394@gmail.com>
2021-11-06 23:16:58 +0530

    Using The Token For Requests To Protected Resources

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1. Added React context to manage state for login and logout on respective successful attempt.
 2. Integrated the concept on AuthForm.js and MainNavigation.js
 3. Added AuthContext provider on index.js
+
+## Using The Token For Requests To Protected Resources (Commit 6)
+
+1. added logic to save token using Context and sending request to change user's password using token
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,40 @@
 import classes from './ProfileForm.module.css';
+import { useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+
+  const newPasswordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDKVPw-pcqAPph6OghWliPzNd06gOcSN14',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          idToken: authCtx.idToken,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => {
+      
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
